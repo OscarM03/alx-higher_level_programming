b48c61158c9fe9f87f05ec508226620b296d55d3
OscarM03 <oscarmutwiri03@gmail.com>
2023-11-06 17:14:56 +0300

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    new_tuple = (length, sentence[0])
-    return new_tuple
+    if sentence:
+        length = len(sentence)
+        new_tuple = (length, sentence[0])
+        return new_tuple
+    else:
+        new_tuple = (0, None)
+        return new_tuple
