4300a3ca320553e1163883c65d585b47a0d4a66a
OscarM03 <oscarmutwiri03@gmail.com>
2023-11-08 20:30:06 +0300

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,16 +1,15 @@
-
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if input_dict:
-        keys_to_delete = []
-        for key, val in input_dict.items():
-            if val == target_value:
-                keys_to_delete.append(key)
-        if keys_to_delete:
-            num_keys_to_delete = len(keys_to_delete)
-            i = 0
-            while num_keys_to_delete > 0:
-                input_dict.pop(keys_to_delete[i])
-                i += 1
-                num_keys_to_delete -= 1
-        return input_dict
+    if a_dictionary:
+        delete = []
+        for c, y in a_dictionary.items():
+            if y == value:
+                delete.append(c)
+        if delete != []:
+            length = len(delete)
+            j = 0
+            while length > 0:
+                a_dictionary.pop(delete[j])
+                j += 1
+                length -= 1
+        return a_dictionary
