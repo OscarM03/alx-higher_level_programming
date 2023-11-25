02278290f8826ad2195790900a91ee46a784da25
OscarM03 <oscarmutwiri03@gmail.com>
2023-11-25 19:22:57 +0300

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
     def __init__(self, size=0):
         """
         Args:
-            size: 
+            size:
                 if not an int, raise TypeError
                 if size < 0, raise ValueError
         """
