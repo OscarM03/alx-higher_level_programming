2e86b19fd3b88f1b08997c8a5e2a027e9dba4d26
OscarM03 <oscarmutwiri03@gmail.com>
2023-11-28 13:50:33 +0300

    task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,19 @@
+class LockedClass:
+    """
+    LockedClass: A class with restricted attribute creation.
+
+    Attributes:
+    - first_name: Allowed attribute for storing the first name.
+    """
+
+    __slots__ = ['first_name']
+
+    def __init__(self, value=""):
+        """
+        Initializes a LockedClass instance.
+
+        Args:
+        - value (str): The initial value for the 'first_name' attribute.
+        """
+        self.first_name = value
+
