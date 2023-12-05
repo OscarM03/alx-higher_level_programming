9245b99ecff6604b1818ac7eba1efe6db2dd6848
OscarM03 <oscarmutwiri03@gmail.com>
2023-12-05 15:25:02 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
-"""No module imported"""
+"""A simple script to read and print the contents of a UTF-8 encoded text file."""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding='utf-8') as file:
-        print(file.read(), end="")
+    """
+    Read the contents of a UTF-8 encoded text file and print it to stdout.
+
+    Parameters:
+    - filename (str): The path to the file to be read. If not provided, the function does nothing.
+
+    Raises:
+    - FileNotFoundError: If the specified file is not found.
+    - UnicodeDecodeError: If there is an issue decoding the file as UTF-8.
+
+    Example:
+    >>> read_file("example.txt")
+    This is the content of the example file.
+
+    """
+    try:
+        with open(filename, "r", encoding='utf-8') as file:
+            print(file.read(), end="")
+    except FileNotFoundError:
+        print(f"Error: File not found - {filename}")
+    except UnicodeDecodeError:
+        print(f"Error: Unable to decode the file as UTF-8 - {filename}")
+
+# Example usage:
+file_path = 'path/to/your/file.txt'
+read_file(file_path)
