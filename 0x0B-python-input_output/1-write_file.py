c893e1eedeb3e615c904415d5f2b102be5025b3c
OscarM03 <oscarmutwiri03@gmail.com>
2023-12-05 15:56:17 +0300

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""Defines a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """opens a text file and write a string to it"""
+    with open(filename, "w") as file:
+      chars_written = file.write(text)
+    return chars_written
