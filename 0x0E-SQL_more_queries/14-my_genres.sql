27fbb1c19f0cc76495c5d9d202c4e9d884da40ec
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-17 23:47:46 +0300

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- lists all genres of dexter
-SELECT gt.name AS name
+SELECT gt.name 
 FROM tv_shows ts
 INNER JOIN tv_show_genres tg ON ts.id = tg.show_id
 INNER JOIN tv_genres gt ON tg.genre_id = gt.id
 WHERE ts.id = 8
-ORDER BY name ASc;
+ORDER BY gt.name ASc;
