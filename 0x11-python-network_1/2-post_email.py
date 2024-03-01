b24bf06eefcc3e63a4f207a677b0388d0dce925b
OscarM03 <oscarmutwiri03@gmail.com>
2024-03-01 18:17:13 +0300

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,12 +5,12 @@ import urllib.request
 import urllib.parse
 
 url = sys.argv[1]
-post_data = {'email':sys.argv[2]}
+post_data = {'email': sys.argv[2]}
 
 encoded_data = urllib.parse.urlencode(post_data).encode('utf-8')
 
 request = urllib.request.Request(url, data=encoded_data, method='POST')
 
 with urllib.request.urlopen(request) as response:
-  req_data = response.read()
-  print(req_data.decode('utf-8'))
+    req_data = response.read()
+    print(req_data.decode('utf-8'))
