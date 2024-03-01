4b231ca52a315555fadac9b95d9f162704052d1c
OscarM03 <oscarmutwiri03@gmail.com>
2024-03-01 21:11:30 +0300

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,13 +4,15 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-post_data = {'email': sys.argv[2]}
 
-encoded_data = urllib.parse.urlencode(post_data).encode('utf-8')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    post_data = {'email': sys.argv[2]}
 
-request = urllib.request.Request(url, data=encoded_data, method='POST')
+    encoded_data = urllib.parse.urlencode(post_data).encode('utf-8')
 
-with urllib.request.urlopen(request) as response:
-    req_data = response.read()
-    print(req_data.decode('utf-8'))
+    request = urllib.request.Request(url, data=encoded_data, method='POST')
+
+    with urllib.request.urlopen(request) as response:
+        req_data = response.read()
+        print(req_data.decode('utf-8'))
