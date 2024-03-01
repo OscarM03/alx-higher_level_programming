576596e1bfa02c7bf23a533743a18aa1633c2d29
OscarM03 <oscarmutwiri03@gmail.com>
2024-03-01 19:30:36 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status using requests library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,9 @@
 """
 import requests
 
-response = requests.get('https://alx-intranet.hbtn.io/status')
-print("Body response:")
-print("\t type: {}".format(type(response.text)))
-print("\t content: {}".format(response.text))
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t type: {}".format(type(response.text)))
+    print("\t content: {}".format(response.text))
