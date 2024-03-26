0b72a2e82fa1675940deaad25fcc0a92d1542387
OscarM03 <oscarmutwiri03@gmail.com>
2024-03-26 15:17:55 +0300

    Getting the status code of request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log('code:', response.statusCode);
 });
